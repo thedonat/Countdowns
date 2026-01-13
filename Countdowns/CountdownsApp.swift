e613814a63bb26149303efa72d96d8238f73ce61
Burak Donat <burak.donat@commencis.com>
2026-01-13 20:37:02 +0300

    Initial Commit

---
+++ Countdowns/CountdownsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownsApp.swift
+//  Countdowns
+//
+//  Created by Burak Donat on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CountdownsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
